eacf552ca37f7edd5969732ebff3dd685c278993
Dave Banwell <dave@davebanwell.com>
2025-01-05 19:23:18 -0500

    completes lab requirements

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,109 @@
+from helpers import validate_attribute
+
 class Article:
+
+    all = []
+
     def __init__(self, author, magazine, title):
+        validate_attribute(title, str, 5, 50)
+        self._title = title
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+        Article.all.append(self)
+
+    def __repr__(self):
+        return (
+            f"<Article: title='{self.title}', author='{self.author.name}', "
+            f"magazine='{self.magazine.name}'>"
+        )
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, author):
+        validate_attribute(author, Author)
+        self._author = author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, magazine):
+        validate_attribute(magazine, Magazine)
+        self._magazine = magazine
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        validate_attribute(name, str, 1)
+        self._name = name   
+
+    def __repr__(self):
+        return f"<Author: name='{self.name}'>"
+
+    @property
+    def name(self):
+        return self._name
 
     def articles(self):
-        pass
+        return [item for item in Article.all if item.author == self]
 
     def magazines(self):
-        pass
+        return list(set(item.magazine for item in self.articles()))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        return list(set(item.category for item in self.magazines())) or None
 
 class Magazine:
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
 
+    def __repr__(self):
+        return f"<Magazine: name='{self.name}', category='{self.category}'>"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        validate_attribute(name, str, 2, 16)
+        self._name = name
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, category):
+        validate_attribute(category, str, 1)
+        self._category = category
+
     def articles(self):
-        pass
+        return [item for item in Article.all if item.magazine == self]
+
+    def articles_by_author(self, author):
+        return [item for item in self.articles() if item.author == author]
 
     def contributors(self):
-        pass
+        return list(set(item.author for item in self.articles()))
 
     def article_titles(self):
-        pass
+        return [article.title for article in self.articles()] or None
 
     def contributing_authors(self):
-        pass
+        return list(set(
+            item for item in self.contributors()
+            if len(self.articles_by_author(item)) > 2
+        )) or None
--- lib/debug.py
+++ lib/debug.py
@@ -5,6 +5,25 @@ from classes.many_to_many import Article
 from classes.many_to_many import Author
 from classes.many_to_many import Magazine
 
+auth1 = Author("Dave Banwell")
+auth2 = Author("Kate Reynolds")
+auth3 = Author("Paul Fedory")
+
+mag1 = Magazine("Gifted", "Education")
+mag2 = Magazine("Kindness", "Sociology")
+mag3 = Magazine("Encode.this", "Computer Science")
+
+art1 = Article(auth1, mag1, "Trials and Triumphs")
+art2 = Article(auth1, mag3, "When is a List NOT a List?")
+art3 = Article(auth2, mag2, "Teaching Kindness Through Song")
+art4 = Article(auth2, mag1, "The Nexus of Neurodivergence and Giftedness")
+art5 = Article(auth3, mag3, "The Emergence of Elixir")
+art6 = Article(auth3, mag1, "The Fear of Being Extraordinary")
+art7 = Article(auth1, mag1, "The Joys of Neurodivergence")
+art8 = Article(auth1, mag1, "A round peg...")
+art9 = Article(auth2, mag2, "The Foundations of Kindness Must be Laid Early")
+art10 = Article(auth2, mag2, "Kindness: a daily practice")
+
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
